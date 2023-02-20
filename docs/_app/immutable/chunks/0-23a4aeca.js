5a67d3703da4af48dd80d0b2c19c6aedcca9a483
Rob Leroy <robleroy@msn.com>
2023-02-20 09:48:50 -0600

    update

--- docs/_app/immutable/chunks/0-23a4aeca.js
+++
@@ -1,1 +0,0 @@
-import{_ as r}from"./_layout-95f8a586.js";import{default as t}from"../components/pages/_layout.svelte-e8a99eba.js";export{t as component,r as universal};
--- docs/service-worker.js
+++ docs/service-worker.js
@@ -1,5 +1,5 @@
 const _ = [
-  "/pwa_test/_app/immutable/chunks/0-23a4aeca.js",
+  "/pwa_test/_app/immutable/chunks/0-b9d0b16d.js",
   "/pwa_test/_app/immutable/chunks/1-92c365ac.js",
   "/pwa_test/_app/immutable/chunks/2-4c5e8afd.js",
   "/pwa_test/_app/immutable/chunks/3-9ae3c8d2.js",
@@ -9,12 +9,12 @@ const _ = [
   "/pwa_test/_app/immutable/chunks/index-f05a2fe5.js",
   "/pwa_test/_app/immutable/chunks/paths-93b45301.js",
   "/pwa_test/_app/immutable/chunks/singletons-3747866d.js",
-  "/pwa_test/_app/immutable/start-0dcb0abc.js",
+  "/pwa_test/_app/immutable/start-7bc884b5.js",
   "/pwa_test/_app/immutable/assets/_error-a61fa775.css",
   "/pwa_test/_app/immutable/components/pages/_error.svelte-4b625053.js",
-  "/pwa_test/_app/immutable/assets/_layout-4b55a68b.css",
+  "/pwa_test/_app/immutable/assets/_layout-d6b9cda8.css",
   "/pwa_test/_app/immutable/modules/pages/_layout.js-11d4e431.js",
-  "/pwa_test/_app/immutable/components/pages/_layout.svelte-e8a99eba.js",
+  "/pwa_test/_app/immutable/components/pages/_layout.svelte-0f2f49db.js",
   "/pwa_test/_app/immutable/components/pages/_page.svelte-d6f6ea2f.js",
   "/pwa_test/_app/immutable/components/pages/about/_page.md-bb8ad695.js",
   "/pwa_test/_app/immutable/components/pages/gh_pages/_page.md-f7198e8e.js",
@@ -32,7 +32,7 @@ const _ = [
   "/pwa_test/pwa_512.png",
   "/pwa_test/pwa_master.svg",
   "/pwa_test/splash.png"
-], o = "1676907683504", c = `cache-${o}`, n = [
+], o = "1676908118703", c = `cache-${o}`, n = [
   ..._,
   // the app itself
   ...l
@@ -40,25 +40,25 @@ const _ = [
 ];
 self.addEventListener("install", (a) => {
   console.log("install");
-  async function e() {
+  async function t() {
     await (await caches.open(c)).addAll(n), console.log("addFilesToCache");
   }
-  a.waitUntil(e());
+  a.waitUntil(t());
 });
 self.addEventListener("activate", (a) => {
   console.log("activate");
-  async function e() {
-    for (const t of await caches.keys())
-      t !== c && await caches.delete(t), console.log("deleteOldCaches");
+  async function t() {
+    for (const e of await caches.keys())
+      e !== c && await caches.delete(e), console.log("deleteOldCaches");
   }
-  a.waitUntil(e());
+  a.waitUntil(t());
 });
 self.addEventListener("fetch", (a) => {
   if (a.request.method !== "GET")
     return;
-  async function e() {
-    const t = new URL(a.request.url), s = await caches.open(c);
-    if (n.includes(t.pathname))
+  async function t() {
+    const e = new URL(a.request.url), s = await caches.open(c);
+    if (n.includes(e.pathname))
       return s.match(a.request);
     try {
       const p = await fetch(a.request);
@@ -67,5 +67,5 @@ self.addEventListener("fetch", (a) => {
       return s.match(a.request);
     }
   }
-  a.respondWith(e());
+  a.respondWith(t());
 });
